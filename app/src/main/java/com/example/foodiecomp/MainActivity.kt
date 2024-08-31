ea66d806d2b13cdbeda4fad0f78a4a4c94a6c3e6
tusharhow <tusharsoftdev@gmail.com>
2024-08-31 20:25:35 +0600

    Unused code removed

--- app/src/main/java/com/example/foodiecomp/MainActivity.kt
+++ app/src/main/java/com/example/foodiecomp/MainActivity.kt
@@ -6,15 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.foodiecomp.src.navigation.SetupNavGraph
-import com.example.foodiecomp.src.views.get_started.GetStartedScreen
-import com.example.foodiecomp.src.views.onboarding.OnboardingPage
-import com.example.foodiecomp.src.views.onboarding.OnboardingScreen
 import com.example.foodiecomp.ui.theme.FoodieCompTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +18,7 @@ class MainActivity : ComponentActivity() {
             FoodieCompTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    var navController = rememberNavController()
+                    val navController = rememberNavController()
                     SetupNavGraph(navController = navController)
                 }
             }
--- app/src/main/java/com/example/foodiecomp/src/views/auth/LoginScreen.kt
+++ app/src/main/java/com/example/foodiecomp/src/views/auth/LoginScreen.kt
@@ -1,18 +1,13 @@
 package com.example.foodiecomp.src.views.auth
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -20,8 +15,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.text.font.FontWeight
+import com.example.foodiecomp.src.components.CustomTextField
 import com.example.foodiecomp.src.components.PrimaryButton
 import com.example.foodiecomp.ui.theme.PrimaryColor
 import com.example.foodiecomp.ui.theme.PrimaryLightColor
